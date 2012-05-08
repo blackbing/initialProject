2b2db21bb6abd1e2071787eb3a29ac21a8fed989
Bingo Yang <blackbing@gmail.com>
2012-05-08 04:38:45 -0400

    remove unneceary script/path

--- build.js
+++ build.js
@@ -3,7 +3,5 @@
   name: "main",
   out: 'javascript/main-built.js',
   paths: {
-    "jquery-mousewheel":'vender/jquery-mousewheel/jquery.mousewheel',
-    "antiscroll":'vender/antiscroll/antiscroll'
   }
 }
